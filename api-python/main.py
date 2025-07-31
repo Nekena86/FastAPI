a9c93b4e60d46e6517d95e98aa10f4735b460b1a
nekena86 <hei.nekena.6@gmail.com>
2025-07-31 09:42:29 +0300

    ajout du code

--- api-python/main.py
+++ api-python/main.py
@@ -1,24 +1,59 @@
+import datetime
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.responses import HTMLResponse
+from typing import List
 
-# Créer une instance FastAPI
+from wsproto.events import TextMessage
+
 app = FastAPI()
 
-# Route GET basique
-@app.get("/")
+post_db: List[object] = []
+
+@app.get("/ping", status_code=200)
 def read_root():
-    return {"message": "Bienvenue dans FastAPI 🎉"}
+    return {"message": "pong"}
 
-# Définir un modèle de données pour POST
-class Item(BaseModel):
-    name: str
-    price: float
+@app.get("/home", status_code=200)
+def read_welcome():
+  return HTMLResponse("""<html>
+        <h1>Welcome home!</h1>
+  </html>""")
 
-# Route POST
-@app.post("/items")
-def create_item(item: Item):
-    return {
-        "name": item.name,
-        "price": item.price,
-        "status": "Item bien reçu ✅"
-    }
+
+class Post(BaseModel):
+    author:str
+    title:str
+    content:str
+    creation_datetime:datetime.datetime
+
+@app.post("/posts", status_code=201)
+def create_post(post: Post):
+    post_db.append(post)
+    return post_db
+
+@app.get("/posts", status_code=200)
+def get_posts():
+    return post_db
+
+@app.put("/posts")
+def add_post(post: Post):
+    if post in post_db:
+        return{"post":post}
+    post_db.append(post)
+    return {"post": post}
+
+
+class User(BaseModel):
+    username:str
+    password:str
+
+@app.get("/ping/auth")
+def auth(user: User):
+    if user.username == "admin" and user.password == "123456":
+        return {"message":read_root()}
+    else:
+        return {"message": "authentification failed"}
+
+
